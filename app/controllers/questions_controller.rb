c4594cbbf70286da57a23d8a7e40c8749588dbc8
Giuseppe Galante <giuseppe.galante@cazelis.com>
2024-03-04 15:08:35 +0100

    Ajout code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,25 @@
 class QuestionsController < ApplicationController
 
 
-def ask
+  def ask
 
+  end
+
+  def answer
+    @question = params[:ask]
+      if @question == "I am going to work"
+        @answer = "Great!"
+      elsif @question.ends_with?("?")
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+  end
 end
 
-def answer
-  @question = params[:ask]
-  @answer = "Great!"
-end
 
-end
+
+
+# Si le message est I am going to work, le coach répondra Great!.
+# Si le message contient un point d’interrogation ? à la fin, le coach répondra Silly question, get dressed and go to work!.
+# Sinon, le coach répondra I don't care, get dressed and go to work!.
